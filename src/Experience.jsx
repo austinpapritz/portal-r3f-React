40a072a8da928daf2702c94e0f70e60c265ee891
Austin Papritz <austin@papritz.dev>
2023-04-03 12:45:38 -0700

    portal light mesh added

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -13,9 +13,12 @@ export default function Experience() {
       <OrbitControls makeDefault />
 
       <Center>
+        {/* This is the baked material for the entire scene, minus the poles which are done separately below */}
         <mesh geometry={nodes.baked.geometry}>
           <meshBasicMaterial map={bakedTexture} />
         </mesh>
+
+        {/* Each pole has a node in the portal.glb, we have to assign the geometry and position, we give it a BasicMaterial and white color */}
         <mesh
           geometry={nodes.poleLightA.geometry}
           position={nodes.poleLightA.position}
@@ -32,6 +35,12 @@ export default function Experience() {
         >
           <meshBasicMaterial color="#ffffe5" />
         </mesh>
+
+        <mesh
+          geometry={nodes.portalLight.geometry}
+          position={nodes.portalLight.position}
+          rotation={nodes.portalLight.rotation}
+        ></mesh>
       </Center>
     </>
   );
